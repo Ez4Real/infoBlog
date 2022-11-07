d36026f80c16aec599606dd575ad238599076c81
Ez4Real <butilka05roma@gmail.com>
2022-11-07 15:02:28 +0200

    mysql connection

--- infoBlog/settings.py
+++ infoBlog/settings.py
@@ -90,12 +90,12 @@ WSGI_APPLICATION = 'infoBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '',
     }
 }
 
