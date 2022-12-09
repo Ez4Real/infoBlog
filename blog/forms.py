269dabe37401ecf444a563f4795ac0960d94b173
Ez4Real <butilka05roma@gmail.com>
2022-12-09 21:44:31 +0200

    join-team form customize

--- blog/forms.py
+++ blog/forms.py
@@ -27,14 +27,23 @@ class ContactForm(forms.Form):
     ('3', _('Doctor or candidate of sciences')),
     ('4', _('Other'))
     )
-    first_name = forms.CharField(label=_('First name'), max_length=100)
-    last_name = forms.CharField(label=_('Last name'), max_length=100)
-    email = forms.EmailField(label='E-mail', max_length=100)
-    education_level = forms.ChoiceField(label = _('Your level of education'), choices=EDUCATION_LEVELS)
+    first_name = forms.CharField(label=_('First name'),
+                                 max_length=100,
+                                 widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
+    last_name = forms.CharField(label=_('Last name'),
+                                max_length=100,
+                                widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
+    email = forms.EmailField(label='E-mail',
+                             max_length=100,
+                             widget=forms.EmailInput(attrs={'type': 'email', 'class': 'form-control'}))
+    education_level = forms.ChoiceField(label = _('Your level of education'),
+                                        choices=EDUCATION_LEVELS,
+                                        widget=forms.Select(attrs={'class': 'form-control'}))
     expertise_area = forms.CharField(label=_('Briefly describe your area of expertise'),
                                      max_length=600,
-                                     widget=forms.Textarea)
+                                     widget=forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}))
     expectations = forms.CharField(label=_('What do you expect from cooperation with us?'),
                                    max_length=600,
-                                   widget=forms.Textarea)
-    resume = forms.FileField(label=_('Resume'))
+                                   widget=forms.Textarea(attrs={'type': 'text', 'class': 'form-control', 'style': 'max-height: 90px'}))
+    resume = forms.FileField(label=_('Resume'),
+                             widget=forms.FileInput(attrs={'class': 'form-control', 'style': 'max-width: 50%'}))
