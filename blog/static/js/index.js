7885c7efc61682ea9f0cef80187a17b839f4c85b
nananseek <nooobleemonk@gmail.com>
2023-02-07 03:50:54 +0200

    add cards for blog

--- blog/static/js/index.js
+++ blog/static/js/index.js
@@ -41,19 +41,15 @@ menu_btn_close.addEventListener("click", () => {
   top_header.classList.toggle("fix-side-opt");
 });
 
-window.addEventListener('load', async () => {
-  if ('serviceWorker' in navigator) {
-    try {
-      registration = await navigator.serviceWorker.register(reg)
-      console.log('Service Worker registered: ', registration);
-    } catch (e){
-      console.error('Service Worker registration failed: ', e);
-    }
-  }
-});
 
-// $.ajaxSetup({
-//   headers: {
-//      'Cache-Control': 'no-cache'
+
+// window.addEventListener('load', () => {
+//   if ('serviceWorker' in navigator) {
+//     try {
+//       registration = navigator.serviceWorker.register(reg)
+//       console.log('Service Worker registered: ', registration);
+//     } catch (e){
+//       console.error('Service Worker registration failed: ', e);
+//     }
 //   }
 // });
